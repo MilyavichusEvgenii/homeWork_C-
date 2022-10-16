0eab431c321449c87d4eb3f5941cff1eb55d4d60
MilyavichusEvgenii <milyavichus@yandex.ru>
2022-10-17 01:14:52 +0300

    task_04

---
+++ home_work_001/task_04/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+ Console.Clear();
+ Console.WriteLine("Введите число");
+ int number = int.Parse(Console.ReadLine());
+ int a = Math.Abs(number);
+
+ string numbStr = "";
+    for(int i = 2; i <= a; i = i + 2){
+        if(number < 0){
+             numbStr+=-i + " ";
+        } else {
+            numbStr+=i + " "; 
+        }
+    }
+ Console.WriteLine($"Все четные числа в промежутке от 1 до {number}: {numbStr}");
