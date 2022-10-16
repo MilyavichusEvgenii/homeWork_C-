58941664f964a5eefe07e74d2ccfeaa9fd7c6ff7
MilyavichusEvgenii <milyavichus@yandex.ru>
2022-10-16 16:38:26 +0300

    task_01

---
+++ home_work_001/task_01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if( number1 > number2) {
+    Console.WriteLine($"Число {number1} больше, чем число {number2}");
+} 
+if (number1 < number2) {
+    Console.WriteLine($"Число {number2} больше, чем число {number1}");
+}
+if (number1 == number2) {
+    Console.WriteLine("Числа равны");
+}
