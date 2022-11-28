0c97da3c199c11556d8f8356d8a6bcd1fec65efc
MilyavichusEvgenii <milyavichus@yandex.ru>
2022-11-28 12:59:01 +0200

    save task_66

--- home_work_009/task_66/Program.cs
+++ home_work_009/task_66/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+
+int GetNumber(string massage)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(massage);
+        if(int.TryParse(Console.ReadLine() ?? "", out int number)){
+            result = number;
+            break;
+        }
+    }
+    return result;
+}
+
+
+string GetRecNumber(int a, int b)
+{
+    if(a == b){
+        return a.ToString();
+    } else {
+        return a + " " + GetRecNumber(a + 1, b);
+    }
+}
+
+int rangeA = GetNumber("Введите число M");
+int rangeB = GetNumber("Введите число N");
+string resultNumbLine = GetRecNumber(rangeA, rangeB);
+Console.WriteLine($"Натуральные числа ({resultNumbLine}) в диапазоне от {rangeA} до {rangeB} включительно");
